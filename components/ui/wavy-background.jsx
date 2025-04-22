65fe2d8313417b7bbd98c8b2a7a04783d6ac3134
Lovedeep <lovedeep24253@gmail.com>
2025-04-22 17:01:11 +0530

    Minor bug fix

--- components/ui/wavy-background.jsx
+++ components/ui/wavy-background.jsx
@@ -13,7 +13,7 @@ import { useRouter } from "next/navigation";
 export const WavyBackground = ({
   children,
   className,
-  containerClassName,
+  containerClassName = "",
   colors,
   waveWidth,
   backgroundFill,
